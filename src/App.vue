6a8cab883d21e305def432f426f3e136e7ee6f47
Matheus Nasc <matheusnascimentox725@gmail.com>
2024-02-11 19:49:40 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">teste</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style lang="scss" scoped>
+.app {
+}
+</style>
---
+++ src/components/atoms/Images.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src="pathImage" class="images" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    src: { type: String, required: true }
+  },
+  computed: {
+    pathImage() {
+      return require(`@/assets/images/${this.src}`)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.images {
+  width: 100%;
+}
+</style>
